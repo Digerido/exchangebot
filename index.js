0e2b67cd52a90ee7539a07e8d90b064798279ddf
Digerido <pecheridzeqqq@mail.ru>
2023-06-20 21:46:06 +0700

    add storage, exceptions and regex

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const getBaseUrl = 'http://127.0.0.1:3000'; // Замените на ваш ба
 const bot = new TelegramBot(token, { polling: true });
 const result = await fetch(`${getBaseUrl}/api/public/valutes`);
 const data = await result.json();
-const Storage = new Map();
+const valuteType = new Map();
+const giveValuteValue = new Map();
+const getValuteValue = new Map();
+let giveValuteKey;
+let getValuteKey;
 
 // 1)При /start отдаем валюту
 async function giveValute(chatId) {
   const valuteList = data.result.filter(valute => valute.isGive === true)
-    .map(valute => ([{ text: valute.title, callback_data: valute.bestchangeKey}]));
-    Storage.set(chatId, 'isGive')
+    .map(valute => ([{ text: valute.title, callback_data: valute.bestchangeKey }]));
+  valuteType.set(chatId, 'isGive')
   const dropdownValuteList = {
     reply_markup: JSON.stringify({
       inline_keyboard: valuteList
@@ -28,7 +32,6 @@ async function giveValute(chatId) {
 async function getValute(chatId) {
   const valuteList = data.result.filter(valute => valute.isGet === true)
     .map(valute => ([{ text: valute.title, callback_data: valute.bestchangeKey }]));
-    Storage.set(chatId, 'isGet')
   const dropdownValuteList = {
     reply_markup: {
       inline_keyboard: valuteList
@@ -38,26 +41,51 @@ async function getValute(chatId) {
 }
 
 // Listen for any kind of message. There are different kinds of messages.
-bot.onText(/\/start/, async (msg) => {
+bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
-  try {
-    giveValute(chatId)
-  } catch (error) {
-    console.error('Error:', error);
+  if (msg.text === '/start') {
+    try {
+      giveValute(chatId)
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  else{
+    const userAmount = parseFloat(msg.text.replace(',', '.'))
+    console.log(userAmount)
+    const give = giveValuteValue.get(chatId);
+    const get = getValuteValue.get(chatId);
+    if(give?.minGive > userAmount){
+      bot.sendMessage(chatId, 'Введенное значение меньше допустимого');
+    }
+    else{
+      bot.sendMessage(chatId, 'Приятно было познакомиться!');
+      
+    }
+    //высчитать курс
+    //else if(get[0].minGive > userAmount){
+    //  bot.sendMessage(chatId, 'Введенное значение меньше допустимого');
+    //}
   }
 })
 
 bot.on('callback_query', (query) => {
   const chatId = query.message.chat.id;
-  const typeValute = Storage.get(chatId)
+  const valute = valuteType.get(chatId)
   let choosenValute = query.data;
-  console.log('typeValute=',typeValute.trim(),'choosenValute = ', choosenValute)
-  if(typeValute === 'isGive'){ 
-    console.log('start pol')
-    bot.sendMessage(chatId, `Вы выбрали вариант: ${choosenValute}`);
+  if (valute === 'isGive') {
+    giveValuteKey = data.result.find(valute => valute.bestchangeKey === choosenValute)
+    bot.sendMessage(chatId, `Вы выбрали вариант хей: ${giveValuteKey.title}`);
     getValute(chatId)
+    valuteType.set(chatId, 'isGet')
   }
-  else if(typeValute === 'isGet'){
-    bot.sendMessage(chatId, `Hello freak bitches! Вы выбрали вариант: ${choosenValute}`);
+  else if (valute === 'isGet') {
+    getValuteKey = data.result.find(valute => valute.bestchangeKey === choosenValute)
+    console.log('getValuteKey =',getValuteKey.title)
+    bot.sendMessage(chatId, `Вы выбрали направление: ${giveValuteKey.title + ' -> ' + getValuteKey.title}. Минимальная сумма к обмену ${giveValuteKey.minGive + ' ' + giveValuteKey.key} или ${getValuteKey.minGive + ' ' + getValuteKey.key} напишите сумму к обмену`);
+    valuteType.set(chatId, 'isReadyToSend');
+    giveValuteValue.set(chatId, giveValuteKey);
+    getValuteValue.set(chatId, getValuteKey);
   }
+
 });
