18023cfc13f5bf1ef7767926891fe4be9a2ef820
Digerido <pecheridzeqqq@mail.ru>
2023-06-21 21:24:32 +0700

    try to add telegraph. Add calculation  and refactoring code

--- index.js
+++ index.js
@@ -4,22 +4,70 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.BOT_TOKEN;
-const getBaseUrl = 'http://127.0.0.1:3000'; // Замените на ваш базовый URL
+const getBaseUrl = process.env.GETBASEURL; // Замените на ваш базовый URL
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
-const result = await fetch(`${getBaseUrl}/api/public/valutes`);
-const data = await result.json();
-const valuteType = new Map();
-const giveValuteValue = new Map();
-const getValuteValue = new Map();
-let giveValuteKey;
-let getValuteKey;
+const valutes = await fetch(`${getBaseUrl}/api/public/valutes`).then(res => res.json());
+const crossRatesList = await fetch(`${getBaseUrl}/api/public/cross-rates`).then(res => res.json());
+let choosenValuteStatus;
+const crossRates = crossRatesList.result.filter((cr) => {
+  return cr.isDisabled === false;
+});
+//const disabledCrossRates = crossRatesList.filter((cr) => {
+//  return cr.isDisabled === true;
+//});
+//Объекты валют
+let giveValute;
+let getValute;
+// для подсчитанные суммы валют
+let giveAmount;
+let getAmount;
+
+const directExchangeRate = () => {
+  if (!giveValute || !getValute) return null;
+  const crossRate = crossRates.find((cr) => {
+    return (
+      cr.from.bestchangeKey === giveValute.bestchangeKey &&
+      cr.to.bestchangeKey === getValute.bestchangeKey
+    );
+  });
+  console.log('crossRate = ',crossRate)
+  if (crossRate) return 1 / crossRate.out;
+  console.log('calc = ',(getValute.percentGet * getValute.course) /
+  (giveValute.percentGive * giveValute.course))
+  return (
+    getValute &&
+    giveValute &&
+    (getValute.percentGet * getValute.course) /
+    (giveValute.percentGive * giveValute.course)
+  );
+};
+
+const getFixFactor = (valute) => {
+  return ["RUB", "USDT"].includes(valute?.key) ? 2 : 5;
+};
+
+
+const calculateGetAmount = () => {
+  return parseFloat(
+    (giveAmount / directExchangeRate()).toFixed(
+      getFixFactor(getValute)
+    )
+  );
+};
+
+
+function setGiveAmount(value) {    
+  const fixedValue = (+value).toFixed(getFixFactor(giveValute));
+  giveAmount = parseFloat(fixedValue);
+  getAmount = calculateGetAmount();
+}
 
 // 1)При /start отдаем валюту
-async function giveValute(chatId) {
-  const valuteList = data.result.filter(valute => valute.isGive === true)
+async function selectGiveValute(chatId) {
+  const valuteList = valutes.result.filter(valute => valute.isGive === true)
     .map(valute => ([{ text: valute.title, callback_data: valute.bestchangeKey }]));
-  valuteType.set(chatId, 'isGive')
+  choosenValuteStatus = 'isGive';
   const dropdownValuteList = {
     reply_markup: JSON.stringify({
       inline_keyboard: valuteList
@@ -29,8 +77,8 @@ async function giveValute(chatId) {
 }
 
 // 2)Получаем валюту
-async function getValute(chatId) {
-  const valuteList = data.result.filter(valute => valute.isGet === true)
+async function selectGetValute(chatId) {
+  const valuteList = valutes.result.filter(valute => valute.isGet === true)
     .map(valute => ([{ text: valute.title, callback_data: valute.bestchangeKey }]));
   const dropdownValuteList = {
     reply_markup: {
@@ -45,47 +93,37 @@ bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   if (msg.text === '/start') {
     try {
-      giveValute(chatId)
+      selectGiveValute(chatId)
     } catch (error) {
       console.error('Error:', error);
     }
   }
-  else{
+  else {
     const userAmount = parseFloat(msg.text.replace(',', '.'))
     console.log(userAmount)
-    const give = giveValuteValue.get(chatId);
-    const get = getValuteValue.get(chatId);
-    if(give?.minGive > userAmount){
+    if (giveValute?.minGive > userAmount) {
       bot.sendMessage(chatId, 'Введенное значение меньше допустимого');
     }
-    else{
-      bot.sendMessage(chatId, 'Приятно было познакомиться!');
-      
+    else {
+      setGiveAmount(userAmount)
+      bot.sendMessage(chatId, `Ваш обмен: ${giveAmount} ${giveValute?.key} к отправке ${getAmount} ${getValute?.key} к получению. Текущий курс ${giveValute?.course}/${getValute?.course}. Курс сделки будет зафиксирован в момент подтверждения отправки средств. Если подтверждаете ответьте - да`);
     }
-    //высчитать курс
-    //else if(get[0].minGive > userAmount){
-    //  bot.sendMessage(chatId, 'Введенное значение меньше допустимого');
-    //}
   }
 })
 
 bot.on('callback_query', (query) => {
   const chatId = query.message.chat.id;
-  const valute = valuteType.get(chatId)
   let choosenValute = query.data;
-  if (valute === 'isGive') {
-    giveValuteKey = data.result.find(valute => valute.bestchangeKey === choosenValute)
-    bot.sendMessage(chatId, `Вы выбрали вариант хей: ${giveValuteKey.title}`);
-    getValute(chatId)
-    valuteType.set(chatId, 'isGet')
-  }
-  else if (valute === 'isGet') {
-    getValuteKey = data.result.find(valute => valute.bestchangeKey === choosenValute)
-    console.log('getValuteKey =',getValuteKey.title)
-    bot.sendMessage(chatId, `Вы выбрали направление: ${giveValuteKey.title + ' -> ' + getValuteKey.title}. Минимальная сумма к обмену ${giveValuteKey.minGive + ' ' + giveValuteKey.key} или ${getValuteKey.minGive + ' ' + getValuteKey.key} напишите сумму к обмену`);
-    valuteType.set(chatId, 'isReadyToSend');
-    giveValuteValue.set(chatId, giveValuteKey);
-    getValuteValue.set(chatId, getValuteKey);
-  }
+  if (choosenValuteStatus === 'isGive') {
+    console.log(choosenValuteStatus)
+    giveValute = valutes.result.find(valute => valute.bestchangeKey === choosenValute)
 
+    bot.sendMessage(chatId, `Вы выбрали вариант: ${giveValute.title}`);
+    selectGetValute(chatId)
+    choosenValuteStatus = 'isGet'
+  }
+  else if (choosenValuteStatus === 'isGet') {
+    getValute = valutes.result.find(valute => valute.bestchangeKey === choosenValute)
+    bot.sendMessage(chatId, `Вы выбрали направление: ${giveValute.title + ' -> ' + getValute.title}. Минимальная сумма к обмену ${giveValute.minGive + ' ' + giveValute.key} или ${getValute.minGive + ' ' + getValute.key} напишите сумму к обмену`);
+  }
 });
